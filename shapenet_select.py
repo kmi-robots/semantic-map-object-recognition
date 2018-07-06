60b4d3c5f798df3b7aa5297b757a92f23e7f4889
Agnese Chiatti <azc76@psu.edu>
2018-07-06 12:54:00 +0100

    Add full ids as well

--- shapenet_select.py
+++ shapenet_select.py
@@ -1,19 +1,22 @@
-#Querying Shapenet Solr Engine, given a list of keywords
-
 import os
 import requests
 import json
 import sys
 
+
 SOLR_BASE='https://www.shapenet.org/solr/models3d/'
 n_rows = 1000
 out_format = 'json'
 
+#Hardcoded keyword list for object models
+#TODO: change to argparse 
 queries =['chair', 'mug', 'monitor', 'plant']
 
 results=[]
 ids=[]
+full_ids=[]
 
+#Querying Shapenet Solr Engine, given a list of keywords
 for q in queries:
 
     data = requests.get(SOLR_BASE+'select?q='+q+'AND+source%3A3dw&rows='+str(n_rows)+'&wt='+out_format)
@@ -22,7 +25,18 @@ for q in queries:
     results.append(records)
 
     ids.extend([record["id"] for record in records])
-      
+    
+    full_ids.extend([record["fullid"] for record in records])
+     
+     
+#Use the same id to retrieve texture image
+
+
+#Import openCV segmentation from other script
+
+
+#Save results for later matching
+
 
 print(len(results))
 print(results[2])

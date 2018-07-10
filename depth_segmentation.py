d8cb703a4e5b1494914bcb423238985a62fc69f7
Agnese Chiatti <azc76@psu.edu>
2018-07-10 17:23:09 +0100

    Read from rosbag as well

--- depth_segmentation.py
+++ depth_segmentation.py
@@ -5,26 +5,118 @@ import argparse
 from PIL import Image
 import numpy as np
 import sys
+from shapenet_select import findAndMatch
+from pylab import arange, array, uint8 
+from matplotlib import pyplot as plt
+import rosbag
+import subprocess, yaml
+
 
 def preproc(img):
+        
+    
+    #img = array(img,dtype=float32)
+    print(img.dtype)
+    img =np.float16(img)
+    img = img*0.001
 
+    print(np.max(img))
+    print(np.min(img))
 
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    
+    #gray = np.uint8(gray)   
+    #ret, markers=  cv2.connectedComponents(gray)
     edges = canny_edge(gray)
 
+    kernel = np.ones((10,10),np.uint8)
+
+    #op =  cv2.morphologyEx(gray, cv2.MORPH_OPEN, kernel)
+    
+    closing =  cv2.morphologyEx(gray, cv2.MORPH_CLOSE, kernel)
+    
+    #dist_transform = cv2.distanceTransform(gray,cv2.DIST_L2,5)
+    jet = cv2.applyColorMap(closing,cv2.DIST_L2,5)
+
+    maxIntensity = 255.0 # depends on dtype of image data
+    x = arange(maxIntensity) 
+
+    # Parameters for manipulating image data
+    phi = 2.0
+    theta = 2.0
+
+    y = (maxIntensity/phi)*(x/(maxIntensity/theta))**0.5
+
+    '''
+    hist,bins = np.histogram(closing.flatten(),256,[0,256])
+    cdf = hist.cumsum()
+    cdf_normalized = cdf * hist.max()/ cdf.max()
+    plt.plot(cdf_normalized, color = 'b')
+    plt.hist(closing.flatten(),256,[0,256], color = 'r')
+    plt.xlim([0,256])
+    plt.legend(('cdf','histogram'), loc = 'upper left')
+    plt.show()
+    '''
+    equ = cv2.equalizeHist(closing)
+    res = np.hstack((closing,equ)) #stacking images side-by-side
+    cv2.imwrite('/mnt/c/Users/HP/Desktop/KMI/out-canny/res-aligned_'+f,res)
+
+    # Decrease intensity such that
+    # dark pixels become much darker, 
+    # bright pixels become slightly dark 
+
+    closingc = (maxIntensity/phi)*(equ/(maxIntensity/theta))**2
+    closingc = array(closingc,dtype=uint8)
+    #blur = cv2.bilateralFilter(closing,5,20,20)
+
+    closing =  cv2.morphologyEx(equ, cv2.MORPH_CLOSE, kernel)
+    #cv2.imwrite('/mnt/c/Users/HP/Desktop/KMI/out-canny/blurred_'+f, blur) 
+    #kernel = np.ones((5,5),np.uint8)
+    op = cv2.morphologyEx(closing, cv2.MORPH_OPEN, kernel)
+    #cv2.imwrite('/mnt/c/Users/HP/Desktop/KMI/out-canny/notcontrasted'+f, closing) 
+    #cv2.imwrite('/mnt/c/Users/HP/Desktop/KMI/out-canny/contrasted'+f, closingc) 
+    
+    canny= cv2.Canny(op, 80, 85)
+
+    jet = cv2.applyColorMap(equ,cv2.DIST_L2,5)
+    cv2.imwrite('/mnt/c/Users/HP/Desktop/KMI/out-canny/jet_equ'+f, jet)
+    #cv2.imwrite('/mnt/c/Users/HP/Desktop/KMI/out-canny/canny_'+f, canny) 
+    __, contours,hierarchy = cv2.findContours(closing, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) 
+    cv2.drawContours(jet, contours, -1, (0, 0, 255), 3)
+
+    cv2.imwrite('/mnt/c/Users/HP/Desktop/KMI/out-canny/jet_cont'+f, jet)
+    #sys.exit(0)
+    #edges=gray
+    #print(cv2.connectedComponents(gray, 4, cv2.CV_32S))
+    #cv2.imwrite('/mnt/c/Users/HP/Desktop/KMI/test',cv2.connectedComponents(gray, 4, cv2.CV_32S))
+    
     return edges
 
 def contour_det(gray_img):
 
     #Binarize 
-    ret,thresh = cv2.threshold(gray_img,127,255,cv2.THRESH_BINARY)    #+cv2.THRESH_OTSU)    
+    #ret,thresh = cv2.threshold(gray_img,127,255,cv2.THRESH_BINARY)    #+cv2.THRESH_OTSU)    
+        
+    kernel = np.ones((5,5),np.uint8)
+   
+    '''    
+    dilation = cv2.dilate(thresh,kernel,iterations = 10)
+    thresh=dilation 
+    '''
     
+    #closing = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+    closing =  cv2.morphologyEx(gray_img, cv2.MORPH_CLOSE, kernel)
+    #op = cv2.morphologyEx(gray_img, cv2.MORPH_OPEN, kernel)
+    thresh=closing
+
     #Find contours 
     #retrieves all of the contours and reconstructs a full hierarchy of nested contours, no approx
-    __, contours,hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE) 
-
+    __, contours,hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE) 
+    
+    cv2.drawContours(thresh, contours, -1, (0, 0, 255), 3)
+    #print(contours)
     return contours
-   
+    
 
 def canny_edge(gray_img):
 
@@ -33,25 +125,33 @@ def canny_edge(gray_img):
 
 def draw_cnt(contours, img, fname, outpath):
 
-    #for cnt in contours:
+    '''
+    conts_new =[]
+
+    
+    for cnt in contours:
         
         #Approximate polygons         
-        #epsilon = 0.1*cv2.arcLength(cnt,True)
-        #approx = cv2.approxPolyDP(cnt,epsilon,True)
+        epsilon = 0.1*cv2.arcLength(cnt,True)
+        approx = cv2.approxPolyDP(cnt,epsilon,True)
 
-    #cv2.drawContours(img, [approx], -1, (0, 0, 255), 3)
-    cv2.drawContours(img, contours, -1, (0, 0, 255), 3)
-    kernel = np.ones((5,5),np.uint8)
-    dilation = cv2.dilate(img,kernel,iterations = 1) 
+        cv2.drawContours(img, [approx], -1, (0, 0, 255), 3)
+
+
+        conts_new.append(approx)    
+    '''
+    conts_new = contours
+    cv2.drawContours(img, conts_new, -1, (0, 0, 255), 3)
     
-    cv2.imwrite(os.path.join(outpath, fname), dilation)
+    cv2.imwrite(os.path.join(outpath, fname), img)
+    
 
-    return
+    return conts_new
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("imgpath", help="Provide path to your depth images")
+    parser.add_argument("imgpath", help="Provide path to your depth images/ or rosbag")
     parser.add_argument("outpath", help="Provide path to output imgs with drawn contours")
     args =parser.parse_args()
 
@@ -59,45 +159,71 @@ if __name__ == '__main__':
 
     files = os.listdir(args.imgpath)
     img_mat =[]
+
+    #Read from image dir
+    if args.imgpath[-3:] != 'bag':
+
+    
+        for f in files:
         
-    for f in files:
-        
-        if f[-3:] == 'ini':
-            #Skip config files, if any
-            continue
+            if f[-3:] == 'ini':
+                #Skip config files, if any
+                continue
         
 
-        try:
+            try:
             #img_array1 = Image.open(os.path.join(args.imgpath,f))   #.convert('L')
             #img_np = np.asarray(img_array1)
 
-            img_array = cv2.imread(os.path.join(args.imgpath,f), cv2.IMREAD_UNCHANGED)
+                 img_array = cv2.imread(os.path.join(args.imgpath,f), cv2.IMREAD_UNCHANGED)
         
-        except Exception as e:
+                 img_mat.append(img_array)
+
+            except Exception as e:
             
-            print("Problem while opening img %s" % f)
-            print(str(e))
+                print("Problem while opening img %s" % f)
+                print(str(e))
             
-            #Skip corrupted or zero-byte images
-            continue
-            
+                #Skip corrupted or zero-byte images
+                continue
+        
+    else:  #or read from rosbag
+    
+        bag= rosbag.Bag(path_to_bag)
+
+        #topics = bag.get_type_and_topic_info()[1].keys()
+        #print(topics)
+        #info_dict = yaml.load(subprocess.Popen(['rosbag', 'info', '--yaml', path_to_bag], stdout=subprocess.PIPE).communicate()[0])
+        #print(info_dict) 
+        img_mat =[msg for topic, msg, t in bag.read_messages()]
+        bag.close()
+
+    for msg in img_mat:
+    
         try:    
 
             #img = cv2.imdecode(img_np, cv2.IMREAD_UNCHANGED)
 
-
             edged =preproc(img_array)
-     
+
+            '''
+            kernel = np.ones((5,5),np.uint8)
             #Segment
             contours = contour_det(edged)
-            #cv2.imwrite('/mnt/c/Users/HP/Desktop/test.png', edges)
-            #print(type(edges))
-            #sys.exit(0)
             
-            draw_cnt(contours, img_array, f, args.outpath)
+            #dil = cv2.dilate(edged,kernel, iterations=2)
+            #opening = cv2.morphologyEx(dil, cv2.MORPH_OPEN, kernel)
+
+            gradient =  cv2.morphologyEx(edged, cv2.MORPH_GRADIENT, kernel)
+            #cv2.imwrite('/mnt/c/Users/HP/Desktop/KMI/out-canny/'+f, gradient)
+            #print(type(edges))
+            sys.exit(0)
+            
+            poly_contours = draw_cnt(contours, img_array, f, args.outpath)
+            findAndMatch(contours2=poly_contours, img2=img_array, fname=f)
             #cv2.imwrite('/mnt/c/Users/HP/Desktop/test.png', gray)
             #cv2.imwrite('/mnt/c/Users/HP/Desktop/test_ng.png', img_array)
-            
+            '''
             #sys.exit(0) 
 
         except Exception as e:
@@ -108,7 +234,6 @@ if __name__ == '__main__':
 
         #Display, with segmentation
 
-        img_mat.append(img_array)
           
 
     #Dump to compressed output for future re-use
--- shapenet_select.py
+++ shapenet_select.py
@@ -2,16 +2,75 @@ import os
 import requests
 import json
 import sys
-from depth_segmentation import preproc, contour_det, draw_cnt
+#from depth_segmentation import preproc
 import cv2
 import numpy as np
 
+
+def findAndMatch(objs=os.listdir('/mnt/c/Users/HP/Desktop/KMI/shapenet-object-proof/merged'), comparout='/mnt/c/Users/HP/Desktop/KMI/test', contours2=None, img2=None, fname=None):
+
+    for o in objs:
+
+        img = cv2.imread(os.path.join(input_p,o), cv2.IMREAD_UNCHANGED)
+        #edges = preproc(img)
+        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        edges = cv2.Canny(gray, 100, 105)
+
+        kernel = np.ones((5,5),np.uint8)
+        dilation = cv2.dilate(edges,kernel,iterations = 1)
+
+        #write partial output
+        #Only external contours instead of full hierarchy here 
+        __, contours,hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE) 
+        imgo = cv2.drawContours(dilation, contours, -1, (0,255,0), 3)
+        #mask = np.zeros(imgo.shape[:2], dtype="uint8") * 255
+        dil = cv2.dilate(imgo, kernel, iterations=1)
+        #cv2.drawContours(mask, contours, -1, (255, 255, 255), -1)
+        #imgm = cv2.bitwise_and(imgo, imgo, mask=mask)
+
+        cv2.imwrite(os.path.join(output_p, o), dil)
+        #Contour detection adds too much noise here
+        #conts = contour_det(edges)
+        #draw_cnt(conts, img, o, output_p)
+        if contours2 is None or img2 is None:
+            continue
+
+        i=1
+        for cnt2 in contours2:
+         
+          
+            cv2.drawContours(img2, contours2[0], -1, (0, 0, 255), 3)
+            #cv2.drawContours(img2, contours2[1], -1, (255, 0, 0), 3)
+            #cv2.drawContours(img2, contours2[2], -1, (0, 255, 0), 3)
+            #print(len(contours2))   
+            
+            cv2.imwrite(os.path.join(comparout,fname[:-4]+"_"+str(i)+fname[-4:]), img2)
+            for cnt in contours:
+        
+                
+                cv2.drawContours(img, [cnt], -1, (0, 0, 255), 3)
+
+                cv2.imwrite(os.path.join(comparout,o[:-4]+"_"+str(i)+o[-4:]), img)
+
+                
+                print("-----Comparison no. %i ----------" % i)    
+                print(cv2.matchShapes(cnt2, cnt,1,0.0))
+            
+                sys.exit(0)
+                i+=1
+      
+        
 SOLR_BASE='https://www.shapenet.org/solr/models3d/'
 n_rows = 1000
 out_format = 'json'
 input_p ='/mnt/c/Users/HP/Desktop/KMI/shapenet-object-proof/merged'
 output_p= '/mnt/c/Users/HP/Desktop/KMI/shapenet-object-proof/merged-out'
 
+if __name__ == '__main__':
+
+
+    objects = os.listdir(input_p)
+    findAndMatch(objects)
 
 '''
 #Hardcoded keyword list for object models
@@ -50,29 +109,7 @@ for q in queries:
 
 #Import openCV segmentation from other script
 
-
-for o in objs:
-
-    img = cv2.imread(os.path.join(input_p,o), cv2.IMREAD_UNCHANGED)
-    edges = preproc(img)
-    kernel = np.ones((5,5),np.uint8)
-    dilation = cv2.dilate(edges,kernel,iterations = 1)
-
-    #write partial output
-    #Only external contours instead of full hierarchy here 
-    __, contours,hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE) 
-    imgo = cv2.drawContours(dilation, contours, -1, (0,255,0), 3)
-    #mask = np.zeros(imgo.shape[:2], dtype="uint8") * 255
-    dil = cv2.dilate(imgo, kernel, iterations=1)
-    #cv2.drawContours(mask, contours, -1, (255, 255, 255), -1)
-    #imgm = cv2.bitwise_and(imgo, imgo, mask=mask)
-    
-    cv2.imwrite(os.path.join(output_p, o), dil)
-    #Contour detection adds too much noise here
-    #conts = contour_det(edges)
-    #draw_cnt(conts, img, o, output_p)
-
-#Save results for later matching
+    #Save results for later matching
 
 
 

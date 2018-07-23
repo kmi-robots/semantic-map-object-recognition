b5b800f988548853fe63eddd67cb1fedd374f65a
Agnese Chiatti <azc76@psu.edu>
2018-07-23 17:48:50 +0100

    Minor changes

--- objTo2D.py
+++ objTo2D.py
@@ -1,39 +1,24 @@
 import os
 import sys
-#import pywavefront
-#import pymesh
 from matplotlib import pyplot as plt
 import meshpy
 import autolab_core
 from perception import CameraIntrinsics
 from PIL import Image
+import numpy as np 
 
-'''
-scene = pywavefront.Wavefront('model_normalized.obj')
+#TODO:
+# scale to more objs
+# keep ids and output in sub-folders grouped by id
 
-
-for name, material in scene.materials.items():
-    print(name)
-    print(material.vertices)
-
-'''
-'''
-#Read full obj file
-with open('model_normalized.obj') as model:
-
-    lines = model.readlines()
-
-#List of vertices 
-vert_raw = [ line.split('\n')[0] for line in lines if line[0] =='v' and line[1]!='n']
-
-print(vert_raw)
-print(len(vert_raw))
-'''
+#Read 3D model
 mesh = meshpy.ObjFile('model_normalized.obj').read()
-#mesh = pymesh.load_mesh('model_normalized.obj')
-#print(mesh)
 
-rt = autolab_core.RigidTransform()
+
+########CAMERA SETUP#################################################################
+#intrinsic matrix values are derived from K matrix in ros message
+#projection matrix is indicated as P in ros
+# (ros has a 3x4 default for P so only the left 3x3 portion has to be considered)
 
 #Camera Intrinsics - based on Kinect 
 ci = CameraIntrinsics(
@@ -43,16 +28,27 @@ ci = CameraIntrinsics(
     fy = 597.6261068216613,
     cx = 324.761877545329,
     cy = 234.3706583714745,
+    #K = np.array([[570.3422241210938, 0.0, 314.5], [ 0.0, 570.3422241210938, 235.5], [0.0, 0.0, 1.0]]),
     skew=0.0,
     height=480,
     width=640
 )
+#######################################################################################
 
+#Defining obj to camera poses
+#z axis away from scene, x to right, y up
+rt = autolab_core.RigidTransform(
+
+    rotation= np.array([[0., 0., -1.], [0., 1., 0.],[1.,0., 0.]]),
+    from_frame='camera',
+    to_frame='obj'
+)
 
 cam = meshpy.VirtualCamera(ci)
 
 rgb_l, depth_l = cam.images(mesh, [rt])
 
+#Output all views to subfolder
 if not os.path.isdir('./faces'):
 
     os.mkdir('./faces')

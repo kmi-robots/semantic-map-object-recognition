430feb2fa20a05e922fd5a0996e8557e706d9348
Agnese Chiatti <azc76@psu.edu>
2018-08-20 17:44:18 +0100

    Link with MatLab script to get segmented areas

--- shape_match.py
+++ shape_match.py
@@ -1,14 +1,17 @@
+import rosbag
+import matlab
+from mat_segmenting import extract_masks
+#import matlab.engine
+import numpy as np
 import os
 import time
 import cv2
 import argparse 
 from PIL import Image
-import numpy as np
 import sys
 #from shapenet_select import findAndMatch
 from pylab import arange, array, uint8 
 from matplotlib import pyplot as plt
-import rosbag
 import subprocess, yaml
 from cv_bridge import CvBridge, CvBridgeError
 import json
@@ -19,6 +22,7 @@ import random as rng
 import scipy.io as sio
 import logging
 import h5py
+
 
 
 def shapeMatch(path_to_models):
@@ -39,10 +43,13 @@ def loadNYU(path_to_mat):
     Loading the NYUDepth label data set  
     from a Matlab file
     '''
+    print('Loading Matlab data set...') 
+
     #matdb = sio.loadmat(path_to_mat)    
     f = h5py.File(path_to_mat,'r')
     variables = f.items()
 
+    
     for var in variables:
 
         #print(var)
@@ -50,6 +57,19 @@ def loadNYU(path_to_mat):
         data = var[1]
         #print ("Name ", name)  # Name
 
+        
+        if type(data) is h5py.Dataset and name=='instances':
+
+            #Use Matlab script from NYUDepth's Toolbox 
+            #to extract masks related to each instance in the scene            
+            instances = data.value            #eng = matlab.engine.start_matlab()
+            #eng.get_instance_masks(instances, nargout=0)   #nargout=0 to avoid output printing within Python
+
+        elif type(data) is h5py.Dataset and name=='labels':
+
+            labels =data.value
+
+        '''
         if type(data) is h5py.Dataset and name=='images':
 
             #print(data.shape)
@@ -61,8 +81,29 @@ def loadNYU(path_to_mat):
 
     
             return data.value
-            
+        '''    
+    
+    print('Label and Instance Matrices collected')
 
+    return labels, instances
+
+
+def get_masks(labels, instances):
+
+    print('Extracting object labels and masks, for each image')
+
+    print(len(zip(labels,instances))) 
+    print(zip(labels,instances)[:1])   
+
+    masks = [extract_masks(matlab.uint8(label_mat.tolist()), matlab.uint8(instance_mat.tolist())) for label_mat, instance_mat in zip(labels,instances)[:1]]
+    
+
+    for in_masks, in_labels in masks:
+
+        print(in_labels)
+        system.exit(0)
+        
+        
 if __name__ == '__main__':
 
 
@@ -73,10 +114,15 @@ if __name__ == '__main__':
     args =parser.parse_args()
 
     start = time.time()
-    
+   
+    print("Perche'?")
+    logging.info('Off we go!')
+ 
     #Imgs are read from the BYUDepth data set here
-    img_mat = loadNYU(args.imgpath)
+    labels, instances = loadNYU(args.imgpath)
 
+    get_masks(labels, instances)
+    '''
     img = img_mat[0]
     #img = np.reshape(img, (480,640,3))
     
@@ -88,7 +134,7 @@ if __name__ == '__main__':
 
     #cv2.imshow('Image', img)    
     cv2.imwrite('./test.png', img.T)   
-
+    '''
 
     
 

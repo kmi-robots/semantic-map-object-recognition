1ba3797de9d257456fa181060e29aba601f99e73
Agnese Chiatti <azc76@psu.edu>
2018-08-22 11:11:22 +0100

    Nailing NYUDepth's namespace

--- shape_match.py
+++ shape_match.py
@@ -45,8 +45,32 @@ def loadNYU(path_to_mat):
     '''
     print('Loading Matlab data set...') 
 
+    
     #matdb = sio.loadmat(path_to_mat)    
     f = h5py.File(path_to_mat,'r')
+
+    label_names = f['names']  
+ 
+    for st in  label_names[0]:
+        #print(st)
+        #print(type(st))
+        obj= f[st]
+
+        str1 = ''.join(chr(i) for i in obj[:])
+
+        if str1 =='chair': 
+            print('Hey, I have just found a chair!')
+   
+    #print(label_names)
+
+    '''
+    for lab in label_names.value:
+        
+        print(lab) 
+        print(f[lab])
+   
+    '''
+    '''
     variables = f.items()
 
     
@@ -57,7 +81,12 @@ def loadNYU(path_to_mat):
         data = var[1]
         #print ("Name ", name)  # Name
 
-        
+
+        if type(data) is h5py.Dataset and name=='names':
+
+            label_names = data.value
+       
+          
         if type(data) is h5py.Dataset and name=='instances':
 
             #Use Matlab script from NYUDepth's Toolbox 
@@ -68,8 +97,7 @@ def loadNYU(path_to_mat):
         elif type(data) is h5py.Dataset and name=='labels':
 
             labels =data.value
-
-        '''
+        
         if type(data) is h5py.Dataset and name=='images':
 
             #print(data.shape)
@@ -81,28 +109,76 @@ def loadNYU(path_to_mat):
 
     
             return data.value
-        '''    
+        
+    '''    
+
+    print('Label names collected')
     
-    print('Label and Instance Matrices collected')
-
-    return labels, instances
+    return label_names
 
 
-def get_masks(labels, instances):
+def get_masks(path_to_masks):
 
+    all_masks=[]
+    fnames = os.listdir(path_to_masks)[:3]
+
+    for filen in fnames:
+
+
+        mat = matlab.engine.start_matlab()
+        f = mat.load(os.path.join(path_to_masks, filen), nargout=1)
+
+        m = f['masks']
+        l = f['lab']
+        all_masks.append((m,l))
+        
+        '''
+        f = h5py.File(os.path.join(path_to_masks,filen),'r')
+
+        variables = f.items()
+
+    
+        for var in variables:
+
+            #print(var)
+            name = var[0]
+            data = var[1]
+
+            if type(data) is h5py.Dataset and name=='masks':
+
+                m = data.value
+            if type(data) is h5py.Dataset and name=='lab':
+
+                l = data.value
+
+        all_masks.append((m,l))
+
+        '''
+    '''
     print('Extracting object labels and masks, for each image')
-
+    
     print(len(zip(labels,instances))) 
     print(zip(labels,instances)[:1])   
 
-    masks = [extract_masks(matlab.uint8(label_mat.tolist()), matlab.uint8(instance_mat.tolist())) for label_mat, instance_mat in zip(labels,instances)[:1]]
+    masks=[]
+    for label_mat, instance_mat in zip(labels,instances):
+
+        #print(label_mat.shape)
+        #print(instance_mat.shape)
+        m, l = extract_masks(matlab.uint8(label_mat.tolist()), matlab.uint8(instance_mat.tolist()))
+                
+        masks.append((m,l))
+        sys.exit(0)
+
+    #masks = [extract_masks(matlab.uint8(label_mat.tolist()), matlab.uint8(instance_mat.tolist())) for label_mat, instance_mat in zip(labels,instances)]
     
 
     for in_masks, in_labels in masks:
 
         print(in_labels)
         system.exit(0)
-        
+    '''
+    return all_masks 
         
 if __name__ == '__main__':
 
@@ -110,18 +186,21 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("imgpath", help="Provide path to the images to be classified") 
     parser.add_argument("modelpath", help="Provide path to the reference models")
+    parser.add_argument("pathtomasks", help="Provide path to the mat files containing masks and label info")
     parser.add_argument("outpath", help="Provide path to output the similarity scores")
     args =parser.parse_args()
 
     start = time.time()
    
-    print("Perche'?")
-    logging.info('Off we go!')
+   
+    #logging.info('Off we go!')
  
     #Imgs are read from the BYUDepth data set here
-    labels, instances = loadNYU(args.imgpath)
-
-    get_masks(labels, instances)
+    names = loadNYU(args.imgpath)
+    #print(names)
+    all_masks = get_masks(args.pathtomasks)
+    print(len(all_masks))  
+    
     '''
     img = img_mat[0]
     #img = np.reshape(img, (480,640,3))

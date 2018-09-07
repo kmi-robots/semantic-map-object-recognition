dbb607f47faacedfcc31fe60b51fe5d90e8c54c0
Agnese Chiatti <azc76@psu.edu>
2018-09-07 11:50:19 +0100

    Clean up a bit / init feature match

--- shape_match.py
+++ shape_match.py
@@ -25,125 +25,64 @@ import h5py
 import csv
 import statistics as stat
 
+
+#### A bunch of ugly hardcoded object IDs    ##############################
+
 chairs=['9e14d77634cf619f174b6156db666192-0.png', '9e14d77634cf619f174b6156db666192-2.png', '9e14d77634cf619f174b6156db666192-5.png', '9e14d77634cf619f174b6156db666192-7.png', '9e14d77634cf619f174b6156db666192-10.png', '9e14d77634cf619f174b6156db666192-12.png', '49918114029ce6a63db5e7f805103dd-0.png', '49918114029ce6a63db5e7f805103dd-1.png' ,'49918114029ce6a63db5e7f805103dd-5 (1).png', '49918114029ce6a63db5e7f805103dd-5.png', '49918114029ce6a63db5e7f805103dd-6.png', '49918114029ce6a63db5e7f805103dd-9.png', '49918114029ce6a63db5e7f805103dd-11.png', '49918114029ce6a63db5e7f805103dd-13.png', 'c.png']
 plants=['4d637018815139ab97d540195229f372-1.png', '4d637018815139ab97d540195229f372-3.png', '4d637018815139ab97d540195229f372-7.png', '4d637018815139ab97d540195229f372-8.png', '4d637018815139ab97d540195229f372-11.png', '4d637018815139ab97d540195229f372-12.png' ]
 bins=['7bde818d2cbd21f3bac465483662a51d-0.png', '7bde818d2cbd21f3bac465483662a51d-3.png', '7bde818d2cbd21f3bac465483662a51d-10.png', '7bde818d2cbd21f3bac465483662a51d-12.png', '8ab06d642437f3a77d8663c09e4f524d-0.png', '8ab06d642437f3a77d8663c09e4f524d-3.png', '8ab06d642437f3a77d8663c09e4f524d-5.png', '8ab06d642437f3a77d8663c09e4f524d-8.png', '8ab06d642437f3a77d8663c09e4f524d-9.png', '8ab06d642437f3a77d8663c09e4f524d-13.png']
 
+#############################################################################
 
 
+def mainContour(image):
 
-def shapeMatch(shape1, model):
+    '''
+    Extract most prominent shape from given image
+    '''
+    ret, thresh = cv2.threshold(image, 0, 255,0)
+    #ret, thresh = cv2.threshold(objimg, 127, 255,1)
+    _, contours,hierarchy = cv2.findContours(thresh,cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-
-    ret, thresh2 = cv2.threshold(model, 127, 255,1)
-    _, contours,hierarchy = cv2.findContours(thresh2,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    
 
     areas = [cv2.contourArea(cnt) for cnt in contours]
    
     idx = areas.index(max(areas))    
 
-    shape2 = contours[idx]
 
-    model  = cv2.cvtColor(model,cv2.COLOR_GRAY2RGB)
+    return contours[idx]
+
+
+
+def shapeMatch(shape1, shape2):
+    
+    '''
+    Performs comparison just based on shape of main contour,
+    regardless of RGB or any other feature
+    '''
+
+
+    #model  = cv2.cvtColor(model,cv2.COLOR_GRAY2RGB)
     #cv2.drawContours(model, [shape2], 0, (0,255,0), 3)
     
     #cv2.imshow('', model)
     #cv2.waitKey(8000)
     #sys.exit(0)
 
-    return cv2.matchShapes(shape1, shape2,1,0.0)
+    return cv2.matchShapes(shape1, shape2,3,0.0)
     
 
 
-def loadNYU(path_to_mat):
+def featureMatch(inimg, refimg):
 
     '''
-    Loading the NYUDepth label data set  
-    from a Matlab file
+    TODO: figure out if it should better have img
+    cropped to contour instead
+
     '''
-    print('Loading Matlab data set...') 
-
-    
-    #matdb = sio.loadmat(path_to_mat)    
-    f = h5py.File(path_to_mat,'r')
-
-    #print(f.keys())
-    ''' 
-    label_names = f['names']  
- 
-    for i, st in  enumerate(label_names[0]):
-        #print(st)
-        #print(type(st))
-        obj= f[st]
-
-        str1 = ''.join(chr(i) for i in obj[:])
-
-        #print(str1)
-        #sys.exit(0)
-
-        if str1 =='chair': 
-
-            label_index=i 
-            print('Hey, I have just found a chair!')
-   
-    #print(label_names)
-    
-   
-    for lab in label_names.value:
         
-        print(lab) 
-        print(f[lab])
-   
-    '''
-    
-    variables = f.items()
 
-    
-    for var in variables:
-
-        #print(var)
-        name = var[0]
-        data = var[1]
-        #print ("Name ", name)  # Name
-
-        '''
-        if type(data) is h5py.Dataset and name=='namesToIds':
-
-            print(data.keys())
-            label_names = data.value
-        
-          
-        if type(data) is h5py.Dataset and name=='instances':
-
-            #Use Matlab script from NYUDepth's Toolbox 
-            #to extract masks related to each instance in the scene            
-            instances = data.value            #eng = matlab.engine.start_matlab()
-            #eng.get_instance_masks(instances, nargout=0)   #nargout=0 to avoid output printing within Python
-
-        elif type(data) is h5py.Dataset and name=='labels':
-
-            labels =data.value
-        '''
-
-        if type(data) is h5py.Dataset and name=='images':
-
-            #print(data.shape)
-            #data.resize((480,640,3,1449))
-            #print(data.shape)
-            # If DataSet pull the associated Data
-            # If not a dataset, you may need to access the element sub-items
-            return data.value
-
-
-        
-    '''   
-    print(label_names)
-    print('Label names collected')
-    
-    return label_index
-    '''
-
+     
         
 if __name__ == '__main__':
 
@@ -173,7 +112,7 @@ if __name__ == '__main__':
     objectpaths = [os.path.join(args.imgpath, name) for name in objectn]
 
     #Recap all in a csv
-    wrtr = csv.writer(open(os.path.join(args.outpath, 'l1_results_recap_chairs.csv'), 'w'))
+    wrtr = csv.writer(open(os.path.join(args.outpath, 'l3_results_recap_bins.csv'), 'w'))
     #Write header
     wrtr.writerow(["imageid", 'category', 'bestmatch', 'score', 'mean', 'median', 'stdev', 'max', 'predicted', 'correct?'])
     
@@ -200,26 +139,8 @@ if __name__ == '__main__':
 
         #Extract shape
         try:
-            ret, thresh = cv2.threshold(objimg, 0, 255,0)
-            #ret, thresh = cv2.threshold(objimg, 127, 255,1)
-            _, contours,hierarchy = cv2.findContours(thresh,cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-
-            areas = [cv2.contourArea(cnt) for cnt in contours]
-   
-            idx = areas.index(max(areas))    
-
-
-            shape1 = contours[idx]
-
-            #img =  cv2.imread(filep, 1)  
-            #cv2.drawContours(img, [shape1], 0, (0,0,255), 3)
-            #cv2.imshow('', img)
-            
-            #cv2.waitKey(5000)
-
-            #cv2.imshow('', thresh)
-            #cv2.waitKey(2000)
+            shape1 = mainContour(objimg)
         except Exception as e:
 
             #Empty masks 
@@ -262,9 +183,14 @@ if __name__ == '__main__':
             #comparison["similarities"] =[]   
 
             mimage = cv2.imread(modelp, 0)
+                         
+            shape2 = mainContour(mimage)
             
-            #Do a pointwise comparison within each couple
-            score= shapeMatch(shape1, mimage)        
+            #Perform a pointwise comparison within each couple
+            #score= shapeMatch(shape1, shape2)        
+            #Crop image 
+            score = featureMatch(objimg, mimage)
+
             comparison["similarity"]= score
             
             #sys.exit(0)
